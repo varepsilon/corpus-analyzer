868090454f8640814f984944b520155ac668e1f5
Aleksandr Chuklin <chuklin@yandex-team.ru>
2011-05-15 19:10:19 +0400

    win -> unix line endings

--- compute_freq.py
+++ compute_freq.py
@@ -1,5 +1,5 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#coding=utf-8
 
 import os
 import sys
@@ -24,32 +24,28 @@ def handle_terms(terms):
     totals = {}
     docs = os.listdir(dataDir)
     for doc_name in docs:
-        doc = open(os.path.join(dataDir, doc_name))
-        for line in doc:
-            line = unicode(line, "utf-8")
-            for term in terms:
-                if term != "" and line.find(" " + term) != -1:
-                    if not tf.has_key(term):
-                        tf[term] = {}
-                    if tf[term].has_key(doc_name):
-                        tf[term][doc_name] += 1
-                    else:
-                        tf[term][doc_name] = 1
+        with open(os.path.join(dataDir, doc_name)) as doc:
+            for line in doc:
+                line = unicode(line, "utf-8")
+                for term in terms:
+                    if term != "" and line.find(" " + term) != -1:
+                        if not tf.has_key(term):
+                            tf[term] = {}
+                        if tf[term].has_key(doc_name):
+                            tf[term][doc_name] += 1
+                        else:
+                            tf[term][doc_name] = 1
     return tf
 
 def main():
-    if len(sys.argv[1]) != 2:
+    if len(sys.argv) != 2:
         print >>sys.stderr, "Incorrect number of arguments!"\
             "Usage: %s file_with words" % sys.argv[0]
-    terms = open(sys.argv[1]).xreadlines()
-    terms = map(lambda x : unicode(x, "utf-8").strip(), terms)
-    close(sys.argv[1])
+    with open(sys.argv[1]) as f:
+        terms = map(lambda x : unicode(x, "utf-8").strip(), f)
     tf = handle_terms(terms)
     printTF(terms, tf)
 
-
 if __name__ == '__main__':
     main()
-
-
 
